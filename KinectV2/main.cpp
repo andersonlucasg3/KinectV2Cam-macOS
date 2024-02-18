af0aceb698aae196f7bc583f9a095d7db5dba16d
Anderson Ramos <anderson.ramos@epicgames.com>
2024-02-18 00:01:54 -0300

    Initial Commit

---
+++ KinectV2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  KinectV2
+//
+//  Created by Anderson Lucas de Castro Ramos on 18/02/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
