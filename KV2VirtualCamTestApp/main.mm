45a6780399c226a5122b905cf66ecb8c5ac13f20
Anderson Ramos <anderson.ramos@epicgames.com>
2024-02-27 01:56:41 -0300

    Using separate main.mm file
    Updating libfreenect2

---
+++ KV2VirtualCamTestApp/main.mm
@@ -0,0 +1,20 @@
+#import "Source/KV2VirtualCamLogging.h"
+#import "Source/KV2VirtualCamProviderSource.h"
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, char *argv[])
+{
+#if DEBUG
+    os_log(KV2VirtualCamLog, "Starting KinectV2 Virtual Camera Extension");
+#endif
+    
+    @autoreleasepool
+    {
+        KV2VirtualCamProviderSource *providerSource = [[KV2VirtualCamProviderSource alloc] initWithClientQueue:nil];
+        [CMIOExtensionProvider startServiceWithProvider:providerSource.provider];
+        CFRunLoopRun();
+    }
+    
+    return 0;
+}
